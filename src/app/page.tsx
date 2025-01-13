680415e68b38939d9f229f578bf8effbe5a65b11
Buddy Agyin <20474715+agyin3@users.noreply.github.com>
2025-01-13 14:43:28 -0700

    comment out unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import { headers } from "next/headers";
+// import { headers } from "next/headers";
 import { Header } from "./_components/header";
-import { isMobile } from "./_utils/isMobile";
+// import { isMobile } from "./_utils/isMobile";
 import { homeNavLinks } from "./_components/header/data";
 import { Hero } from "./_sections/hero";
 
 export default async function Home() {
-    const headersList = await headers();
-    const userAgent = headersList.get("user-agent");
-    const usesMobile = isMobile(userAgent || "");
+    // const headersList = await headers();
+    // const userAgent = headersList.get("user-agent");
+    // const usesMobile = isMobile(userAgent || "");
     return (
         <div className="min-h-screen">
             <Header navLinks={homeNavLinks} />
